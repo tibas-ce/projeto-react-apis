c2763775c22aa3a6dcb845da4340927357494fdf
Tiberio Ferreira <tiberio.ferreiracs@gmail.com>
2023-09-21 15:53:54 -0300

    Adicionando pokemons na pokedex é atualizando Home page

--- src/Pages/PokemonsListPage/PokemonsListPage.js
+++ src/Pages/PokemonsListPage/PokemonsListPage.js
@@ -3,22 +3,30 @@ import { getAllPokemons } from "../../API/requests";
 import PokemonCard from "../../Components/PokemonCard/PokemonCard";
 import { PokemonListContainer } from "./style";
 
-const PokemonsListPage = () => {
+const PokemonsListPage = ({ pokedex, setPokedex, addPokemon }) => {
   const [pokemons, setPokemons] = useState([]);
+  
   useEffect(() => {
     getAllPokemons(setPokemons);
   }, []);
-  
+
+  const filteredPokemons = pokemons.filter(
+    (pokemon) => !pokedex.find((poke) => poke.name === pokemon.name)
+  )
+
   return (
     <PokemonListContainer>
-      {
-        pokemons.map((poke) => {
-            return(
-                <PokemonCard key={poke.id} poke={poke} />
-            )
-        })
-      }
-      
+      {filteredPokemons.map((poke) => {
+        return (
+          <PokemonCard
+            key={poke.id}
+            poke={poke}
+            pokedex={pokedex}
+            setPokedex={setPokedex}
+            addPokemon={addPokemon}
+          />
+        );
+      })}
     </PokemonListContainer>
   );
 };
