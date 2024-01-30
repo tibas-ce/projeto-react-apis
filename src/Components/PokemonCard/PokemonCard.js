cec9a852598c68fc2dedd86c845f4fa506a17917
Tiberio Ferreira <tiberio.ferreiracs@gmail.com>
2024-01-30 10:11:23 -0300

    estilizando Poke Card

--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -1,36 +1,54 @@
 import {
+  ButtonCaptureRemove,
   ButtonContainer,
+  ButtonDetails,
   ImgContainer,
+  InfoContainer,
   PokeCardContainer,
   PokeImg,
+  TypeImg,
+  PokeBallImg
 } from "./style";
 import { useLocation, useNavigate } from "react-router-dom";
 import { goToDetailPage } from "../../Router/coordinator";
+import { returnTypes } from "../../utils/returnTypes";
+import { returnBackgroundColorCard } from "../../utils/returnBackgroundColorCard";
+import pokeball from "../../assets/pokeball.png"
 
 const PokemonCard = ({ poke, addPokemon, removePokemon }) => {
   const navigate = useNavigate();
   const {pathname} = useLocation();
 
   return (
-    <PokeCardContainer>
+    <PokeCardContainer type={returnBackgroundColorCard(poke.types[0].type.name)} >
+      <InfoContainer>
+        <p>#{poke.id}</p>
+        <h1>{poke.name}</h1>
+        <div>
+          {poke.types.map((type) => {
+            return <TypeImg src={returnTypes(type.type.name)} alt="Tipos"/>
+          })}
+        </div>
+      </InfoContainer>
       <ImgContainer>
+        <PokeBallImg src={pokeball} all="poke-bola" />  
         <PokeImg
-          src={poke.sprites.front_default}
+          // Quando temos separação de palavras em uma propriedade de um objeto para acessar usamos colchetes e transformamos em string
+          src={poke.sprites.other["official-artwork"].front_default}
           alt={`imagem do ${poke.name}`}
         />
       </ImgContainer>
       <ButtonContainer>
+        <ButtonDetails onClick={() => goToDetailPage(navigate, poke.name)}>
+          <u>Detalhes</u>
+        </ButtonDetails>
         {
           pathname === "/" ? (
-            <button onClick={() => addPokemon(poke)}>Adicionar</button>
+            <ButtonCaptureRemove onClick={() => addPokemon(poke)}>Capturar!</ButtonCaptureRemove>
           ) : (
-            <button onClick={() => removePokemon(poke.name)}>Remover</button>
+            <ButtonCaptureRemove onClick={() => removePokemon(poke.name)}>Remover</ButtonCaptureRemove>
           )
         }
-        
-        <button onClick={() => goToDetailPage(navigate, poke.name)}>
-          Ver Detalhes
-        </button>
       </ButtonContainer>
     </PokeCardContainer>
   );
